faa7ed78602c4ab6167b555380fb00f5568286e9
Nermin <sehicnermin97@gmail.com>
2025-01-12 21:42:19 +1100

    cleanup template code

---
+++ src/config.ts
@@ -0,0 +1,39 @@
+
+export const site = {
+  name: "Nermin Sehic",
+  email: "sehicnermin97@gmail.com",
+  numPostsOnHomepage: 3,
+  numWorksOnHomepage: 2,
+  numProjectsOnHomepage: 3,
+};
+
+export const home = {
+  title: "Home",
+  description: "Astro Nano is a minimal and lightweight blog and portfolio.",
+};
+
+export const blog = {
+  title: "Blog",
+  description: "A collection of articles on topics I am passionate about.",
+};
+
+export const work = {
+  title: "Work",
+  description: "Where I have worked and what I have done.",
+};
+
+export const projects = {
+  title: "Projects",
+  description: "A collection of my projects, with links to repositories and demos.",
+};
+
+export const socials = [
+  {
+    name: "github",
+    href: "https://github.com/nermin-io"
+  },
+  { 
+    name: "linkedin",
+    href: "https://www.linkedin.com/in/nsehic",
+  }
+];
