1b4dd626fb5b7527886f5d143ed6a90921b5256e
Nermin <sehicnermin97@gmail.com>
2025-01-13 21:26:26 +1100

    Installer prettier and fix config

--- src/config.ts
+++ src/config.ts
@@ -1,39 +1,41 @@
-
-export const site = {
-  name: "Nermin Sehic",
-  email: "sehicnermin97@gmail.com",
-  numPostsOnHomepage: 3,
-  numWorksOnHomepage: 2,
-  numProjectsOnHomepage: 3,
-};
-
-export const home = {
-  title: "Home",
-  description: "Astro Nano is a minimal and lightweight blog and portfolio.",
-};
-
-export const blog = {
-  title: "Blog",
-  description: "A collection of articles on topics I am passionate about.",
-};
-
-export const work = {
-  title: "Work",
-  description: "Where I have worked and what I have done.",
-};
-
-export const projects = {
-  title: "Projects",
-  description: "A collection of my projects, with links to repositories and demos.",
-};
-
-export const socials = [
-  {
-    name: "github",
-    href: "https://github.com/nermin-io"
+export default {
+  content: {
+    homepage: {
+      numPosts: 3,
+      numWork: 2,
+      numProjects: 2,
+    }
   },
-  { 
-    name: "linkedin",
-    href: "https://www.linkedin.com/in/nsehic",
-  }
-];
+  site: {
+    name: "Nermin Sehic",
+    email: "sehicnermin97@gmail.com",
+  },
+  meta: {
+    home: {
+      title: "Home",
+      description: "Software Engineer passionate about Cloud infrastructure, web development, and coffee!"
+    },
+    blog: {
+      title: "Blog",
+      description: "A collection of articles on topics I am passionate about."
+    },
+    projects: {
+      title: "Projects",
+      description: "Where I have worked and what I have done."
+    },
+    work: {
+      title: "Work",
+      description: "Work"
+    }
+  },
+  socials: [
+    {
+      name: "github",
+      href: "https://github.com/nermin-io"
+    },
+    {
+      name: "linkedin",
+      href: "https://www.linkedin.com/in/nsehic",
+    }
+  ]
+};
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,24 +1,25 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import * as config from "@config";
+import config from "@config";
 
 type Context = {
-  site: string
-}
+  site: string;
+};
 
 export async function GET(context: Context) {
-  const blog = (await getCollection("blog"))
-  .filter(post => !post.data.draft);
+  const blog = (await getCollection("blog")).filter((post) => !post.data.draft);
 
-  const projects = (await getCollection("projects"))
-    .filter(project => !project.data.draft);
+  const projects = (await getCollection("projects")).filter(
+    (project) => !project.data.draft,
+  );
 
-  const items = [...blog, ...projects]
-    .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
+  const items = [...blog, ...projects].sort(
+    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
+  );
 
   return rss({
-    title: config.home.title,
-    description: config.home.description,
+    title: config.meta.home.title,
+    description: config.meta.home.description,
     site: context.site,
     items: items.map((item) => ({
       title: item.data.title,
